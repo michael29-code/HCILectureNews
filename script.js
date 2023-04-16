66bdcd8ac0718b39b68c4c02897d1dfd0d510894
michael <alf.michael29@gmail.com>
2023-04-16 21:52:40 +0700

    profile menu

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const navigationhamburger = document.getElementById('hamburger')
 const sidebarcontent = document.getElementById('sidebarContainer')
 const blurcontent = document.getElementById('blurcontent')
 const backbutton = document.getElementById('backbtn')
+const profileicon = document.getElementById('profile-icon')
+const profilemenu = document.getElementById('profilemenu')
 
 
 navigationhamburger.addEventListener('click', () =>{
@@ -13,4 +15,15 @@ blurcontent.addEventListener('click', () => {
 })
 backbutton.addEventListener('click', () => {
     sidebarcontent.style.display = 'none'
+})
+
+profileicon.addEventListener('click', () => {
+    if (profileicon.classList.contains('clicked')) {
+        profilemenu.style.display = 'none'
+        profileicon.classList.remove('clicked')
+    }else if(!profileicon.classList.contains('clicked'))
+    {
+        profileicon.classList.add('clicked')
+        profilemenu.style.display = 'flex'
+    }
 })
